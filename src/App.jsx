7dd5ba675e2b78204c72cd8dc57d2ff91a403a47
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2021-12-17 13:27:37 +0100

    resolve import app.css error

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ import Header from './components/Header';
 import HomePage from './components/HomePage';
 import ProjectsList from './components/ProjectsList';
 
-import './styles/App.css';
-
 function App() {
   return (
     <div className="App">
